5de0e132124ddb97d614148a4bb0f05f5ca81293
Ravindhra Kotla <ravindhrakotla@gmail.com>
2018-11-26 14:11:02 -0500

    Initial Commit after restoring Nuget Packages

---
+++ Vasavi Kirana/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vasavi_Kirana.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
